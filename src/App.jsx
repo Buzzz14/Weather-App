d3ad6dc864178bd0099fed7adaf5c04da3f79da0
Buzzz14 <sushanbajracharya14@gmail.com>
2024-11-06 13:16:58 +0545

    Minor changes.

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,9 @@ function App() {
           <h2 className="text-3xl font-medium text-center">Weather App</h2>
         </nav>
       </header>
+      
       <WeatherApp/>
+      
       <footer className="p-8 bg-n-blue text-l-gray">
         <p className="text-center">&copy; 2024 Weather App </p>
       </footer>
